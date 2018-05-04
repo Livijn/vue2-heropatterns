3f956cabf852a017b43e1f64ebab979911a4bdf8
Steve McDougall <juststeveking@gmail.com>
2018-05-04 14:33:28 +0100

    testing new pattern

--- src/mixins/mixin.js
+++ src/mixins/mixin.js
@@ -5,12 +5,12 @@ export const mixin = {
         background: {
             required: false,
             type: String,
-            default: '#DFDBE5'
+            default: 'DFDBE5'
         },
         foreground: {
             required: false,
             type: String,
-            default: '#9C92AC'
+            default: '9C92AC'
         },
         opacity: {
             required: false,
@@ -26,11 +26,19 @@ export const mixin = {
 
     computed: {
         style () {
-            return `background-color: ${this.color};background-image: url("${this.imageUri}")`
+            return `background-color: %23${this.color}; background-image: url("${this.imageUri}")`
         },
 
         imageUri () {
-            return `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='${this.currentPattern.width}' height='${this.currentPattern.height}' viewBox='0 0 ${this.currentPattern.width} ${this.currentPattern.height}'%3E%3Cg fill='%23${this.foreground}' fill-opacity='${this.opacity}'%3E%3Cpath fill-rule='${this.currentPattern.fillRule}' d='${this.currentPattern.drawing}'/%3E%3C/g%3E%3C/svg%3E`
+            return `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='${this.currentPattern.width}' height='${this.currentPattern.height}' viewBox='0 0 ${this.currentPattern.width} ${this.currentPattern.height}'%3E%3Cg fill='%23${this.foreground}' fill-opacity='${this.opacity}'%3E%3Cpath ${this.fillRule} d='${this.currentPattern.drawing}'/%3E%3C/g%3E%3C/svg%3E`
+        },
+
+        fillRule () {
+            if (this.currentPattern.fillRule) {
+                return `fill-rule='${this.currentPattern.fillRule}'`
+            } else {
+                return ``
+            }
         },
 
         currentPattern () {
