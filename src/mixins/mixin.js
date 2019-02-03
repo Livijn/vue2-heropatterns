7a254b5491ab6b9be2967f06c44b27d29464b529
Fredrik Livijn <ouff@live.se>
2019-02-03 17:10:06 +0100

    fix bug with background color

--- src/mixins/mixin.js
+++ src/mixins/mixin.js
@@ -25,12 +25,20 @@ export const mixin = {
     },
 
     computed: {
+        backgroundColor() {
+            return this.background.replace('#', '');
+        },
+        
+        foregroundColor() {
+            return this.foreground.replace('#', '');
+        },
+        
         style () {
-            return `background-color: %23${this.background}; background-image: url("${this.imageUri}")`
+            return `background-color: #${this.backgroundColor}; background-image: url("${this.imageUri}")`
         },
 
         imageUri () {
-            return `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='${this.currentPattern.width}' height='${this.currentPattern.height}' viewBox='0 0 ${this.currentPattern.width} ${this.currentPattern.height}'%3E%3Cg fill='%23${this.foreground}' fill-opacity='${this.opacity}'%3E%3Cpath ${this.fillRule} d='${this.currentPattern.drawing}'/%3E%3C/g%3E%3C/svg%3E`
+            return `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='${this.currentPattern.width}' height='${this.currentPattern.height}' viewBox='0 0 ${this.currentPattern.width} ${this.currentPattern.height}'%3E%3Cg fill='%23${this.foregroundColor}' fill-opacity='${this.opacity}'%3E%3Cpath ${this.fillRule} d='${this.currentPattern.drawing}'/%3E%3C/g%3E%3C/svg%3E`
         },
 
         fillRule () {
