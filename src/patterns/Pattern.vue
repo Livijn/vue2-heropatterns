c08c2cc46dbbea6ae665baa33cdc405e1b495280
Fredrik Livijn <ouff@live.se>
2019-02-03 21:32:55 +0100

    fixed more patterns

--- src/patterns/Pattern.vue
+++ src/patterns/Pattern.vue
@@ -5,9 +5,52 @@
 </template>
 
 <script>
-    import { mixin } from './../mixins/mixin'
+    import patterns from './../config/patterns'
 
     export default {
-        mixins: [mixin]
+        props: {
+            background: {
+                required: false,
+                type: String,
+                default: 'FFFFFF'
+            },
+            foreground: {
+                required: false,
+                type: String,
+                default: '000000',
+            },
+            opacity: {
+                required: false,
+                type: Number,
+                default: 0.05,
+            },
+            pattern: {
+                required: false,
+                type: String,
+                default: null,
+            }
+        },
+        computed: {
+            backgroundColor() {
+                return '#' + this.background.replace('#', '');
+            },
+
+            foregroundColor() {
+                return this.foreground.replace('#', '');
+            },
+
+            selectedPattern() {
+                return this.pattern && patterns[this.pattern]
+                    .replace('FILLCOLOR', '%23' + this.foregroundColor)
+                    .replace('FILLOPACITY', this.opacity);
+            },
+
+            style() {
+                return {
+                    backgroundColor: this.backgroundColor,
+                    backgroundImage: this.selectedPattern,
+                };
+            },
+        }
     }
 </script>
